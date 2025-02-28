59892731e576618a65f20743ceb2f2273207400f
Konstantin Läufer <laufer@cs.luc.edu>
2025-02-27 18:05:31 -0600

    attempted to fix Zotero download script
    Signed-off-by: Konstantin Läufer <laufer@cs.luc.edu>

--- tools/get-zotero-bibtex.sh
+++ tools/get-zotero-bibtex.sh
@@ -4,20 +4,15 @@
 
 
 BIB_DIR=_bibliography
-TBIB_DIR=$TMPDIR/$BIB_DIR
 ZOTERO_BIB_URLS=_data/zotero-bibs.txt
 ZOTERO_API=https://api.zotero.org
 
 mkdir -p $BIB_DIR
-mkdir -p $TBIB_DIR
-
-echo "BIB_DIR=$BIB_DIR"
-echo "TBIB_DIR=$TBIB_DIR"
 
 for u in $(cut -d/ -f 5-6 $ZOTERO_BIB_URLS) ; do
 
   GROUP_ID="${u%/*}"
-  OUTPUT_FILE="$TBIB_DIR/${u#*/}.bib"
+  OUTPUT_FILE="$BIB_DIR/${u#*/}.bib"
 
   # To ensure correct export of TechReport and PhDThesis entries, 
   # use format=bibtex, NOT biblatex
@@ -28,13 +23,13 @@ for u in $(cut -d/ -f 5-6 $ZOTERO_BIB_URLS) ; do
 done
 
 echo "Sanitizing downloaded bib files"
-for bibfile in $TBIB_DIR/*.bib
+for bibfile in $BIB_DIR/*.bib
 do
     bibfile_raw=${bibfile%.bib}-raw.bib
     mv "$bibfile" "$bibfile_raw"
     tools/sanitize-zotero-bib.py "$bibfile_raw" "$bibfile"
 done
-rm -f $TBIB_DIR/*-raw.bib
+rm -f $BIB_DIR/*-raw.bib
 
 echo "Collating downloaded bibs"
-cat $TBIB_DIR/*.bib > $BIB_DIR/papers.bib
+cat $BIB_DIR/*.bib > $BIB_DIR/papers.bib
